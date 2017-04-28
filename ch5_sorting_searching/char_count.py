2767247ce07f4c7d84eee820fa57c934aa5b60f7
Peace <peacepius716@gmail.com>
2017-04-28 12:27:08 -0400

    passed all tests except last one

--- ch5_sorting_searching/char_count.py
+++ ch5_sorting_searching/char_count.py
@@ -5,24 +5,27 @@ def char_counts(s):
     Return a list containing counts of the number of occurances of each
     ASCII character in s.
 
-      >>> result = char_counts("this is seriously silly")
-      >>> result[ord('t')]
-      1
-      >>> result[ord('i')]
-      4
-      >>> result[ord('2')]
-      5
-      >>> result2 = char_counts("abbcccddddeeeeeffffff")
-      >>> result2[96:105]
+      >>> result1 = char_counts("abbcccddddeeeeeffffff")
+      >>> len(result1)
+      256
+      >>> result1[96:105]
       [0, 1, 2, 3, 4, 5, 6, 0, 0]
+      >>> result2 = char_counts("AACEEEGIIJLLL")
+      >>> result2[64:78]
+      [0, 2, 0, 1, 0, 3, 0, 1, 0, 2, 1, 0, 3, 0]
+      >>> result3 = char_counts("this is seriously silly")
+      >>> result3[ord('t')]
+      1
+      >>> result3[ord('i')]
+      4
+      >>> result3[ord('2')]
+      5
     """
-    count = 0
+    counts = [0] * 256
+
     for ch in s:
-        if letter == ch:
-            ord(ch)
-            count += 1
-            chr(ch)
-    return count
+        counts[ord(ch)] += 1
+    return counts
 
 if __name__ == '__main__':
     import doctest
